3ab8c27b338ac1a3a81feac3184b884a7cd9451f
Doniyor <doniyor_otamurodov@epam.com>
2022-06-05 00:59:14 +0500

    main/feat: application setup
    Initial app commit. Migrated files from initial starter application to current react appliction.
    
    Changes:
    * css root variables
    * assets
    * initial app component

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <span>spli</span>
+        <span>tter</span>
+      </header>
+      <div className="app-container"></div>
+    </div>
+  );
+}
+
+export default App;
