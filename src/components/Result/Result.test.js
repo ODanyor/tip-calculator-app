a3bd0375a34e5f771a553cf6b7fcb3ad508c387d
Doniyor <doniyor_otamurodov@epam.com>
2022-06-05 15:43:18 +0500

    dev/test: Result component test coverage

--- src/components/Result/Result.test.js
+++ src/components/Result/Result.test.js
@@ -1,8 +1,18 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Result from './Result';
 
+const testProps = {
+  name: 'test-name-value',
+  desc: 'test-desc-value',
+  value: 10,
+};
+
 describe('Result component:', () => {
-  it('shoult render properly', () => {
-    render(<Result />);
+  it('should render properly', () => {
+    render(<Result {...testProps} />);
+
+    expect(screen.getByText(testProps.name)).toBeDefined();
+    expect(screen.getByText(`/ ${testProps.desc}`)).toBeDefined();
+    expect(screen.getByText(`$${testProps.value.toFixed(2)}`)).toBeDefined();
   });
 });
