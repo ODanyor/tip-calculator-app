137d178598a27756fb25e93daf0fe82f9fc3b9b8
Doniyor <doniyor_otamurodov@epam.com>
2022-06-06 04:49:25 +0500

    dev/test: Icon component test coverage

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -5,14 +5,14 @@ import './Icon.css';
 function Icon({ name }) {
   const iconClassName = mergeModifiers('icon', name);
 
-  return <span className={iconClassName}></span>;
+  return <span data-testid="icon" className={iconClassName}></span>;
 }
 
 Icon.propTypes = {
   name: PropTypes.oneOf([
     'dollar',
     'person',
-  ]),
+  ]).isRequired,
 };
 
 export default Icon;
--- src/components/Icon/Icon.test.js
+++ src/components/Icon/Icon.test.js
@@ -1,8 +1,32 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Icon from './Icon';
 
 describe('Icon component:', () => {
-  it('should render properly', () => {
-    render(<Icon />);
+  it('should render proerly', () => {
+    render(<Icon name="dollar" />);
+
+    const icon = screen.getByTestId('icon');
+
+    expect(icon.getAttribute('class')).toBe('icon icon--dollar');
+  });
+
+  describe('when name prop value is dollar', () => {
+    it('should render dollar icon', () => {
+      render(<Icon name="dollar" />);
+
+      const icon = screen.getByTestId('icon');
+
+      expect(icon.getAttribute('class')).toContain('icon--dollar');
+    });
+  });
+
+  describe('when name prop value is person', () => {
+    it('should render person icon', () => {
+      render(<Icon name="person" />);
+
+      const icon = screen.getByTestId('icon');
+
+      expect(icon.getAttribute('class')).toContain('icon--person');
+    });
   });
 });
