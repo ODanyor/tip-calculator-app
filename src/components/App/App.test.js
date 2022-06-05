8ed53adf811516db93de2af38d64b88c73d36fe0
Doniyor <doniyor_otamurodov@epam.com>
2022-06-06 04:53:54 +0500

    dev/test: App component test coverage

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,11 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+jest.mock('../Form', () => () => <span>mock-form-component</span>);
+jest.mock('../Display', () => () => <span>mock-display-component</span>);
+
 describe('App Component:', () => {
-  it('should render header text', () => {
+  it('should render app header', () => {
     render(<App />);
 
     expect(screen.getByText('spli')).toBeDefined();
     expect(screen.getByText('tter')).toBeDefined();
+  });
+
+  it('should render app modules', () => {
+    render(<App />);
+
+    expect(screen.getByText('mock-form-component')).toBeDefined();
+    expect(screen.getByText('mock-display-component')).toBeDefined();
   });
 });
