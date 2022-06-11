90563a5b0e609a52c3ac81279a723881c265b6e1
Doniyor <doniyor_otamurodov@epam.com>
2022-06-11 20:43:24 +0500

    dev/test: Results calculations test coverage

--- src/components/Results/Results.test.js
+++ src/components/Results/Results.test.js
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react';
 import Results from './Results';
 import { AppContextProvider } from '../../context/AppContext';
 
-const renderResutls = (props = {}) => render(
-  <AppContextProvider>
+const renderResutls = ({ context, ...props } = {}) => render(
+  <AppContextProvider context={context}>
     <Results {...props} />
   </AppContextProvider>
 );
@@ -12,8 +12,22 @@ describe('Results component:', () => {
   it('should render properly', () => {
     renderResutls();
 
+    const [tipResult, totalResult] = screen.getAllByTestId('result-value');
+
+    expect(tipResult.textContent).toBe('$0.00');
+    expect(totalResult.textContent).toBe('$0.00');
+
     expect(screen.getByText('Tip Amount')).toBeDefined();
     expect(screen.getByText('Total')).toBeDefined();
     expect(screen.getByTestId('button')).toHaveTextContent('Reset');
+  });
+
+  it('should calculated tip and total from context values', () => {
+    renderResutls({ context: [{ bill: '100', tip: '5', people: '2'}, jest.fn()] });
+
+    const [tipResult, totalResult] = screen.getAllByTestId('result-value');
+
+    expect(tipResult.textContent).toBe('$2.50');
+    expect(totalResult.textContent).toBe('$52.50');
   });
 });
